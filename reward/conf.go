fa1c835b1825c10a1bf995e5d8d01a96402b2e49
clov616 <128493967+clov616@users.noreply.github.com>
2023-03-31 14:26:01 +0800

    Update conf.go

--- reward/conf.go
+++ reward/conf.go
@@ -17,7 +17,7 @@ type Env struct {
 
 var (
 	// 默认配置
-	keyWords  = []string{"a", "AB", "bingNew", "iaimi", "爱美", "孤独摇滚", "b站", "bilibili", "大佐de苦de手", "柚子社"}
+	keyWords  = []string{"impact", "Apex", "csgo", "steam", "碧蓝航线", "bingNew", "千恋万花", "BangDream", "孤独摇滚", "b站", "bilibili", "switch", "柚子社"}
 	defaultIP = "[14.102.128.0,2.59.154.0]"
 )
 
